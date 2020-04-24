4d7a15168ccea998373f8efc41168afdc2424055
Yoann L'hostis <yolhostis@live.fr>
2020-04-24 17:13:50 +0200

    initialisation du squelette MVC

---
+++ views/home_view.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+
+    <?php include_once 'views/includes/head.php'?>
+
+    <title><?= ucfirst($page) ?></title>
+</head>
+
+<body>
+
+    <?php include_once 'views/includes/header.php'?>
+
+    <!-- CONTENU -->
+
+    <?php echo "loulou" ?>
+
+    <?php include_once 'views/includes/footer.php'?>
+
+</body>
+</html>
