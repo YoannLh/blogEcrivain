cb39dd97dbb929ca06bdc6588910cc2b32dc50bc
Yoann L'hostis <yolhostis@live.fr>
2020-04-24 19:49:06 +0200

    injection thème bootstrap 'blog'

--- views/includes/head.php
+++ views/includes/head.php
@@ -8,6 +8,7 @@
 <meta name="Reply-to" content="<?= WEBSITE_AUTHOR_MAIL?>">
 <meta name="Copyright" content="<?= WEBSITE_AUTHOR?>">
 <meta name="Language" content="<?= WEBSITE_LANGUAGE?>">
+<meta name="generator" content="Jekyll v3.8.6">
 
 <!-- Open Graph tags -->
 <meta property="og:type"              content="website" />
@@ -15,9 +16,37 @@
 <meta property="og:title"             content="<?= WEBSITE_FACEBOOK_NAME?>" />
 <meta property="og:description"       content="<?= WEBSITE_FACEBOOK_DESCRIPTION?>" />
 <meta property="og:image"             content="<?= WEBSITE_FACEBOOK_IMAGE?>" />
+ 
+<title>Jean Richenoire</title>
 
-<!-- CSS Styles -->
-<link rel="stylesheet" href="<?= PATH?>assets/styles/css/styles.css"/>
+<!-- Bootstrap core CSS -->
+
+<!-- Favicons -->
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+	<link rel="stylesheet" href="<?= PATH?>assets/styles/bootstrap/bootstrap.css"/>
+  	<!-- CSS Styles -->
+	<link rel="stylesheet" href="<?= PATH?>assets/styles/css/styles.css"/>
+
 
 
 <!-- JS Files -->
